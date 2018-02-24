7b792bc4bfc602c5b106b492e12ae7f26c59576e
Jose Manuel <josemanuel.pla@gmail.com>
2018-02-24 21:24:34 +0100

    creamos los metodos para su test

--- CCuenta/src/TestCcuentaJMPR.java
+++ CCuenta/src/TestCcuentaJMPR.java
@@ -1,32 +1,54 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 public class TestCcuentaJMPR {
 
+Ccuenta miCuenta; 
+	
+	@Before
+	public void inicioPruebas(){
+		miCuenta= new Ccuenta("Javier","Aznar", 3200, 1);
+	}	
+	@After
+	public void finPruebas(){
+		miCuenta=null;
+	}
+	
 	@Test
-	public void testCcuenta() {
-		fail("No implementado aun");
+	public void testcrearCuentaJMPR(){			
+		assertNotNull(miCuenta);		
 	}
 
 	@Test
-	public void testGetSaldo() {
-		fail("No implementado aun");
+	public void testgetSaldoJMPR(){				
+		assertEquals(3200,miCuenta.getSaldo(),0);
 	}
 
 	@Test
-	public void testIngresar() {
-		fail("No implementado aun");
+	public void testingresaYsaldoJMPR() throws Exception{
+		miCuenta.ingresar(400);
+		assertEquals(3600,miCuenta.getSaldo(),0);		
 	}
 
 	@Test
-	public void testRetirar() {
-		fail("No implementado aun");
+	(expected=Exception.class)    //Pruebas para ver que las excepciones  funcionan
+	public void testIngresarJMPR() throws Exception{
+		miCuenta.ingresar(-600);		
+	}
+	
+	@Test
+	public void testRetirarJMPR() throws Exception{
+		miCuenta.retirar(600);
+		assertEquals(2600,miCuenta.getSaldo(),0);			
 	}
 
 	@Test
-	public void testGetCuenta() {
-		fail("No implementado aun");
+	(expected=Exception.class)   //Pruebas para ver que las excepciones funcionan
+	public void testRetirarPorEncimaJMPR() throws Exception{
+		miCuenta.retirar(4000);		
 	}
 
 }
