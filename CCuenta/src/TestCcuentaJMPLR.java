c8d0caf2254e1a18cb66b1de2461b1523d6db742
Jose Manuel <josemanuel.pla@gmail.com>
2018-02-24 20:58:53 +0100

    Creamos formulario para el test de JUnit

---
+++ CCuenta/src/TestCcuentaJMPLR.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCcuentaJMPLR {
+
+	@Test
+	public void testGetSaldo() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testIngresar() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testRetirar() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testGetCuenta() {
+		fail("No implementado aun");
+	}
+
+}
